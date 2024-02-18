5150d4a78c3f75960e88fb42c3b320faa57cfac2
James <jamesrobertsongroves@gmail.com>
2024-02-18 03:08:13 +0000

    styles connection

--- app/graphql/types/category_type.rb
+++ app/graphql/types/category_type.rb
@@ -3,6 +3,7 @@ class Types::CategoryType < Types::BaseObject
   field :id, ID, null: false
   field :name, String, null: false
   field :description, String, null: false
+  field :styles, [Types::StyleType], null: true
   field :created_at, GraphQL::Types::ISO8601DateTime, null: false
   field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
 end
